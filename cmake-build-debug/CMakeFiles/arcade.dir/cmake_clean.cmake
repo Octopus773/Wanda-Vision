bd86cc86d61149a757c2bf3b4c4332d43748e781
Zoe Roux <zoe.roux@sdg.moe>
2021-03-15 17:54:44 +0100

    Adding a cmake

---
+++ cmake-build-debug/CMakeFiles/arcade.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arcade.dir/main.cpp.o"
+  "arcade"
+  "arcade.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arcade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
