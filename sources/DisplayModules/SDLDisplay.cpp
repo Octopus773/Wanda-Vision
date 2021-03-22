8261dfbfc1109067ba0c4010a617fb302377df2e
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-03-22 11:47:55 +0100

    updating namespace for Module.hpp starting implementing init of SDL2

--- sources/DisplayModules/SDLDisplay.cpp
+++ sources/DisplayModules/SDLDisplay.cpp
@@ -3,9 +3,41 @@
 //
 
 #include "SDLDisplay.hpp"
+#include <SDL2/SDL.h>
+#include <iostream>
 
 namespace Arcade
 {
+
+	bool SDLDisplay::init()
+	{
+		if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+		{
+			std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+			return false;
+		}
+		this->window = SDL_CreateWindow(this->windowTitle.c_str(),
+										  SDL_WINDOWPOS_UNDEFINED,
+										  SDL_WINDOWPOS_UNDEFINED,
+										  this->windowWidth,
+										  this->windowHeight,
+										  SDL_WINDOW_SHOWN);
+		if(window == nullptr)
+		{
+			std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		}
+		return true;
+	}
+
+	bool SDLDisplay::close()
+	{
+		return false;
+	}
+
+	bool SDLDisplay::shouldClose()
+	{
+		return false;
+	}
 
 	std::list<Event> Arcade::SDLDisplay::pullEvents()
 	{
