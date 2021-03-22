322667f3f9e1869e9797a5ba52e09dfbb11d0778
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-03-22 15:50:27 +0100

    update from Interface Arcade repo

--- sources/Common/Module.hpp
+++ sources/Common/Module.hpp
@@ -9,31 +9,34 @@
 
 #include "ModInfo.hpp"
 
-//! @brief A base module interface
-class IModule
+namespace Arcade
 {
-public:
-	//! @brief Virtual destructor
-	virtual ~IModule() = default;
 
-	//! @brief Initialize this library. (Create windows & so on)
-	//! @return True if the initialization was successful. False otherwise.
-	virtual bool init() = 0;
+	//! @brief A base module interface
+	class IModule
+	{
+	public:
+		//! @brief Virtual destructor
+		virtual ~IModule() = default;
 
-    //! @brief Allow to say to the library  t shutdown and free it's content
-	//! @return True if the shutdown was successful. False otherwise.
-	virtual bool close() = 0;
+		//! @brief Initialize this library. (Create windows & so on)
+		//! @return True if the initialization was successful. False otherwise.
+		virtual bool init() = 0;
 
-	//! @brief Should this module be closed?
-	//! @return True if this module should be closed. False otherwise.
-	virtual bool shouldClose() = 0;
-};
+		//! @brief Allow to say to the library  t shutdown and free it's content
+		//! @return True if the shutdown was successful. False otherwise.
+		virtual bool close() = 0;
 
+		//! @brief Should this module be closed?
+		//! @return True if this module should be closed. False otherwise.
+		virtual bool shouldClose() = 0;
+	};
+}
 
 //! @brief Get the library's header.
 //! @info Used to verify the integrity of the lib.
-extern "C" struct ModInfo getHeader();
+extern "C" struct Arcade::ModInfo getHeader();
 
 //! @brief Get the module class.
 //! @return A new instance of a IDisplayModule or IGameModule.
-extern "C" IModule *getModule();
+extern "C" Arcade::IModule *getModule();
