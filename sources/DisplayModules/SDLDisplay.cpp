9750b7c2c57aa495c1af18e9bc88a3e79da593c3
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-03-22 10:39:48 +0100

    Target is compiling fine

--- sources/DisplayModules/SDLDisplay.cpp
+++ sources/DisplayModules/SDLDisplay.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "SDLDisplay.hpp"
+
+namespace Arcade
+{
+
+	std::list<Event> Arcade::SDLDisplay::pullEvents()
+	{
+		return std::list<Event>();
+	}
+
+	void SDLDisplay::drawLine(GameObjects::LineObject obj)
+	{
+
+	}
+
+	void SDLDisplay::drawRectangle(GameObjects::RectangleObject obj)
+	{
+
+	}
+
+	void SDLDisplay::drawCircle(GameObjects::CircleObject obj)
+	{
+
+	}
+
+	void SDLDisplay::drawSprite(GameObjects::SpriteObject obj)
+	{
+
+	}
+
+	void SDLDisplay::refresh() const
+	{
+
+	}
+}
--- sources/DisplayModules/SDLDisplay.hpp
+++ sources/DisplayModules/SDLDisplay.hpp
@@ -4,12 +4,39 @@
 
 #pragma once
 
-#include "Common/IDisplayModule.hpp"
+#include "IDisplayModule.hpp"
 namespace Arcade
 {
-
+	//! @brief Implementation of the SDL Graphic Library
 	class SDLDisplay : public IDisplayModule {
+	public:
+		SDLDisplay() = default;
+		~SDLDisplay() override =  default;
+		SDLDisplay(const SDLDisplay &) = default;
+		SDLDisplay &operator=(const SDLDisplay &) = default;
 
+		//! @brief Pull events (Keys, Clicks, Closes...)
+		//! @return The list of events that occured.
+		std::list<Event> pullEvents() override;
+
+		//! @brief Draw a line.
+		//! @param obj The line to draw.
+		void drawLine(GameObjects::LineObject obj) override;
+
+		//! @brief Draw a rectangle.
+		//! @param obj The rectangle to draw.
+		void drawRectangle(GameObjects::RectangleObject obj) override;
+
+		//! @brief Draw a circle.
+		//! @param obj The circle to draw.
+		void drawCircle(GameObjects::CircleObject obj) override;
+
+		//! @brief Draw a sprite.
+		//! @param obj The sprite to draw.
+		void drawSprite(GameObjects::SpriteObject obj) override;
+
+		//! @brief Refresh the screen, clear left-overs and draw requested objects.
+		void refresh() const override;
 	};
 
 }
