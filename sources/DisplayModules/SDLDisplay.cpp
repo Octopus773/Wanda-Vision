97ebfc03beb834463239957b3a464b34e52264fd
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-03-22 09:36:38 +0100

    first branch commit

---
+++ sources/DisplayModules/SDLDisplay.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cbihan on 3/22/21.
+//
+
+#include "SDLDisplay.hpp"
---
+++ sources/DisplayModules/SDLDisplay.hpp
@@ -0,0 +1,15 @@
+//
+// Created by cbihan on 3/22/21.
+//
+
+#pragma once
+
+#include "Common/IDisplayModule.hpp"
+namespace Arcade
+{
+
+	class SDLDisplay : public IDisplayModule {
+
+	};
+
+}
