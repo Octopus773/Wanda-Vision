f2bf096e22ca32652d04ff440cbc2e18d23dc1f9
Zoe Roux <zoe.roux@sdg.moe>
2021-03-22 15:55:25 +0100

    Using unique ptr for game objects.

--- sources/Common/IGameModule.hpp
+++ sources/Common/IGameModule.hpp
@@ -28,7 +28,7 @@ namespace Arcade
 
 		//! @brief Return a list of game objects to display.
 		//! @return The list of objects
-		virtual const std::vector<GameObject> &getObjects() = 0;
+		virtual const std::vector<std::unique_ptr<GameObject>> &getObjects() = 0;
 
 		//! @brief Advance the game of x seconds
 		//! @param tick The number of ticks that occured since the last call.
