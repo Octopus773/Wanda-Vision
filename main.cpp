1052e24a4d35980b29f8656538595207e25cc3bd
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-03-30 00:11:58 +0200

    fix compil (test badge CI)

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 		return usage(argv[0]);
 	try {
-		Runner runner(argv[1])
+		Runner runner(argv[1]);
 		return runner.runShell();
 	} catch(const std::exception &ex) {
 		std::cerr << ex.what() << std::endl;
