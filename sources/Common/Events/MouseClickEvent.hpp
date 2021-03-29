c2bc524dfbe7c75f320bcd9a7fca6e3e30a2f9ff
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-03-29 15:41:18 +0200

    adding an enum for the Mouse Button and updating a doc

--- sources/Common/Events/MouseClickEvent.hpp
+++ sources/Common/Events/MouseClickEvent.hpp
@@ -14,11 +14,20 @@ namespace Arcade::Events
 	//! @brief Event called after the mouse clicked (KeyDown, Hold or KeyUp)
 	struct MouseClickEvent : public Event
 	{
+		//! @brief The enum for mouse buttons to allow standardized values between graphical libs
+		enum MouseButton {
+			LEFT = 0,
+			MIDDLE = 1,
+			RIGHT = 2,
+			XBUTTON1 = 3,
+			XBUTTON2 = 4
+		};
+
 		//! @brief The x position (in percentage)
 		unsigned x;
 		//! @brief The y position (in percentage)
 		unsigned y;
-		//! @brief The ID of this click.
-		unsigned id;
+		//! @brief The button of this click.
+		MouseButton button;
 	};
 }
