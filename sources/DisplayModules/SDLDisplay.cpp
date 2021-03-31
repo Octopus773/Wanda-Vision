0ea282e2c329953d6ff333eee374d25e78dac376
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-03-31 16:25:25 +0200

    removing included include and disabling build ci for arcade_sdl2.so

--- sources/DisplayModules/SDLDisplay.cpp
+++ sources/DisplayModules/SDLDisplay.cpp
@@ -7,7 +7,6 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_keycode.h>
 #include <iostream>
 #include "Common/Events/MouseClickEvent.hpp"
 #include "Common/Events/KeyBoardEvent.hpp"
