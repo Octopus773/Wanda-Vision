261be5bfc868e8eccbf120b2cc43de0a694863e5
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-04-08 12:07:36 +0200

    fixing some problems

--- sources/DisplayModules/SFML/SFMLDisplay.cpp
+++ sources/DisplayModules/SFML/SFMLDisplay.cpp
@@ -304,7 +304,6 @@ namespace Arcade
 	{
 		sf::Texture t;
 		sf::Font f;
-		sf::Music m;
 
 		if (type == resourceSpriteType) {
 			if (!t.loadFromFile(path)) {
@@ -317,7 +316,7 @@ namespace Arcade
 			}
 			return f;
 		} else if (type == resourceMusicType) {
-			throw ResourceCreationFailure("Unsupported Music");
+			std::cerr << "Music is unsupported" << std::endl;
 		} else {
 			throw ResourceCreationFailure("Unknown type");
 		}
@@ -332,7 +331,9 @@ namespace Arcade
 		}
 		try {
 			resource = this->createResource(type, path);
-		} catch (const ResourceCreationFailure &) { }
+		} catch (const ResourceCreationFailure &) {
+			return false;
+		}
 		this->_loadedResources[path] = std::make_pair(type, resource);
 		return true;
 	}
