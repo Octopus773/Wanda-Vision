2a49d3abf5913f10cb3fc62c61caf791cc3b43d2
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-04-09 09:32:45 +0200

    first

---
+++ sources/Games/Nibbler/Nibbler.cpp
@@ -0,0 +1,489 @@
+//
+// Created by Zoe Roux on 3/29/21.
+//
+
+#include "Common/Drawables/Rectangle.hpp"
+#include "Common/Drawables/Sprite.hpp"
+#include "Common/Drawables/Text.hpp"
+#include "Common/Drawables/Line.hpp"
+#include "Nibbler.hpp"
+#include "Common/Events/KeyBoardEvent.hpp"
+#include "Exceptions/WrongMapChar.hpp"
+#include <iostream>
+#include <Common/Drawables/Circle.hpp>
+
+namespace Arcade::Nibbler
+{
+	bool Nibbler::init()
+	{
+		this->_resources.emplace_back(std::make_pair("sprite", "assets/pacman/pacman.png"));
+		this->_resources.emplace_back(std::make_pair("sprite", "assets/pacman/blinky.png"));
+		this->_resources.emplace_back(std::make_pair("sprite", "assets/pacman/inky.png"));
+		this->_resources.emplace_back(std::make_pair("sprite", "assets/pacman/clyde.png"));
+		this->_resources.emplace_back(std::make_pair("font", "assets/fonts/angelina.ttf"));
+
+		this->_playerDrawable = Drawables::Sprite();
+		this->_playerDrawable.sizeY = mapTileLength;
+		this->_playerDrawable.sizeX = mapTileLength;
+		this->_playerDrawable.color = 0;
+		this->_playerDrawable.rotation = 0;
+		this->_playerDrawable.x = this->_playerPosition.first;
+		this->_playerDrawable.y = this->_playerPosition.second;
+		this->_playerDrawable.path = "assets/pacman/pacman.png";
+		Drawables::Circle fallbackCircle;
+		fallbackCircle.x = this->_playerPosition.first;
+		fallbackCircle.y = this->_playerPosition.second;
+		fallbackCircle.size = 2;
+		fallbackCircle.color = 0xFFFB00FF;
+
+		Drawables::Rectangle fallbackRectangle;
+		fallbackRectangle.x = this->_playerPosition.first - (mapTileLength / 2);
+		fallbackRectangle.y = this->_playerPosition.second - (mapTileLength / 2);
+		fallbackRectangle.endX = fallbackRectangle.x + mapTileLength;
+		fallbackRectangle.endY = fallbackRectangle.y + mapTileLength;
+		fallbackRectangle.color = 0xFFFB00FF;
+		fallbackCircle.fallback = std::make_shared<Drawables::Rectangle>(fallbackRectangle);
+		this->_playerDrawable.fallback = std::make_shared<Drawables::Circle>(fallbackCircle);
+
+		this->_scoreDrawable = Drawables::Text();
+		this->_scoreDrawable.path = "assets/fonts/angelina.ttf";
+		this->_scoreDrawable.fontSize = 30;
+		this->_scoreDrawable.color = 0xFFFFFFFF;
+		this->_scoreDrawable.x = 5;
+		this->_scoreDrawable.y = 5;
+		this->_scoreDrawable.text = "Score: ";
+		this->_startGame();
+		return true;
+	}
+
+	bool Nibbler::close()
+	{
+		return true;
+	}
+
+	void Nibbler::_startGame()
+	{
+		this->_drawables.clear();
+		this->_map.clear();
+		this->_shouldClose = false;
+		this->_map = this->_createMapFromVector({
+			                                        "  wwwwwwwwwwwwwwwwwww  ",
+			                                        "  wP.......w.......Pw  ",
+			                                        "  w.ww.www.w.www.ww.w  ",
+			                                        "  w.................w  ",
+			                                        "  w.ww.w.wwwww.w.ww.w  ",
+			                                        "  w....w...w...w....w  ",
+			                                        "  wwww.www.w.www.wwww  ",
+			                                        "     w.w.......w.w     ",
+			                                        "wwwwww.w.wwwww.w.wwwwww",
+			                                        "w .......wIBCw....... w",
+			                                        "wwwwww.w.wwwww.w.wwwwww",
+			                                        "__   w.w.......w.w   __",
+			                                        "  wwww.www.w.www.wwww  ",
+			                                        "  w....w...w...w....w  ",
+			                                        "  w.ww.w.wwwww.w.ww.w  ",
+			                                        "  w........ ........w  ",
+			                                        "  w.ww.www.w.www.ww.w  ",
+			                                        "  wP.......w.......Pw  ",
+			                                        "  wwwwwwwwwwwwwwwwwww  "
+		                                        }, mapOffsetTileY, mapOffsetTileX);
+
+		this->_gameScore = 0;
+		this->_playerPosition = {50, 20 * mapTileLength + (mapTileLength / 2)};
+		this->_moves = {0};
+		this->_playerMovement = {0, 0};
+		this->_playerDrawable.rotation = 0;
+	}
+
+	bool Nibbler::shouldClose()
+	{
+		return this->_shouldClose;
+	}
+
+	ModInfo::Modtype Nibbler::getType() const
+	{
+		return ModInfo::GAME;
+	}
+
+	const std::vector<std::pair<std::string, std::string>> &Nibbler::getResources() const
+	{
+		return this->_resources;
+	}
+
+	const std::vector<std::unique_ptr<Drawables::ADrawable>> &Nibbler::getDrawables()
+	{
+		this->_drawables.clear();
+		this->_playerDrawable.x = this->_playerPosition.first;
+		this->_playerDrawable.y = this->_playerPosition.second;
+		this->_playerDrawable.fallback->x = this->_playerPosition.first;
+		this->_playerDrawable.fallback->y = this->_playerPosition.second;
+		if (auto fallback = dynamic_cast<Drawables::Rectangle *>(this->_playerDrawable.fallback->fallback.get())) {
+			fallback->x = this->_playerPosition.first - (mapTileLength / 2);
+			fallback->y = this->_playerPosition.second - (mapTileLength / 2);
+			fallback->endX = fallback->x + mapTileLength;
+			fallback->endY = fallback->y + mapTileLength;
+		}
+
+		this->_drawables.push_back(std::make_unique<Drawables::Sprite>(this->_playerDrawable));
+		for (const auto &i : this->_map) {
+			this->_drawables.emplace_back(std::make_unique<Drawables::Sprite>(i));
+		}
+		this->_drawables.push_back(std::make_unique<Drawables::Text>(this->_scoreDrawable));
+		return this->_drawables;
+	}
+
+	const std::vector<Sound> &Nibbler::getSounds()
+	{
+		return this->_sounds;
+	}
+
+	void Nibbler::addTicks(unsigned int tick)
+	{
+		this->_processPlayerMovement(tick);
+		this->_processScore();
+		this->_shouldClose = this->_isGameEnded();
+		this->_moves.moveX = 0;
+		this->_moves.moveY = 0;
+	}
+
+	void Nibbler::restart()
+	{
+		this->_startGame();
+	}
+
+	void Nibbler::handleEvent(Event &event)
+	{
+		try {
+			auto key = dynamic_cast<Events::KeyboardEvent &>(event);
+			if (key.type == Event::KeyUp || key.type == Event::KeyDown)
+				return;
+			switch (key.key) {
+			case Events::KeyboardEvent::UP_ARROW:
+			case Events::KeyboardEvent::KEY_Z:
+				this->_moves.moveY = std::min(-1, this->_moves.moveY + 1);
+				break;
+			case Events::KeyboardEvent::DOWN_ARROW:
+			case Events::KeyboardEvent::KEY_S:
+				this->_moves.moveY = std::max(1, this->_moves.moveY - 1);
+				break;
+			case Events::KeyboardEvent::RIGHT_ARROW:
+			case Events::KeyboardEvent::KEY_D:
+				this->_moves.moveX = std::min(1, this->_moves.moveX + 1);
+				break;
+			case Events::KeyboardEvent::LEFT_ARROW:
+			case Events::KeyboardEvent::KEY_Q:
+				this->_moves.moveX = std::max(-1, this->_moves.moveX - 1);
+				break;
+			default:
+				return;
+			}
+		}
+		catch (const std::bad_cast &) { }
+	}
+
+	unsigned long Nibbler::getScore()
+	{
+		return this->_gameScore;
+	}
+
+	std::vector<Drawables::Sprite> Nibbler::_createMapFromVector(const std::vector<std::string> &map, int hOffset, int vOffset)
+	{
+		std::vector<Drawables::Sprite> ret;
+		int xIndex = -1;
+		int yIndex = -1;
+
+		for (const auto &i : map) {
+			yIndex++;
+			xIndex = -1;
+			for (const auto &j : i) {
+				xIndex++;
+				if (j == MapChar::NOTHING) {
+					continue;
+				}
+				ret.emplace_back(this->_getSpriteFromChar(j, xIndex + vOffset, yIndex + hOffset));
+			}
+		}
+		return ret;
+	}
+
+	Drawables::Rectangle Nibbler::_getRectangleFromChar(char c, int xIndex, int yIndex)
+	{
+		Drawables::Rectangle rect;
+
+		switch (c) {
+		case MapChar::WALL:
+			rect.x = xIndex * mapTileLength;
+			rect.y = yIndex * mapTileLength;
+			rect.endX = rect.x + mapTileLength;
+			rect.endY = rect.y + mapTileLength;
+			rect.color = mapWallColor;
+			return rect;
+		case MapChar::HIDE_RECTANGLE:
+			rect.x = xIndex * mapTileLength;
+			rect.y = (yIndex - 3) * mapTileLength;
+			rect.endX = rect.x + mapTileLength;
+			rect.endY = rect.y + (4 * mapTileLength);
+			rect.color = 0x000000FF;
+			return rect;
+		case MapChar::BIG_PACGUM:
+			rect.x = (xIndex * mapTileLength) + 1;
+			rect.y = (yIndex * mapTileLength) + 1;
+			rect.endX = rect.x + mapTileLength - 2;
+			rect.endY = rect.y + mapTileLength - 2;
+			rect.color = 0xFFFFFFFF;
+			return rect;
+		case MapChar::SMALL_PACGUM:
+			rect.x = (xIndex * mapTileLength) + 1;
+			rect.y = (yIndex * mapTileLength) + 1;
+			rect.endX = rect.x + mapTileLength - 2;
+			rect.endY = rect.y + mapTileLength - 2;
+			rect.color = 0x777777FF;
+			return rect;
+		case MapChar::BLINKY:
+			rect.x = xIndex * mapTileLength + 1;
+			rect.y = yIndex * mapTileLength + 1;
+			rect.endX = rect.x + mapTileLength - 2;
+			rect.endY = rect.y + mapTileLength - 2;
+			rect.color = 0xFF0000FF;
+			return rect;
+		case MapChar::INKY:
+			rect.x = xIndex * mapTileLength + 1;
+			rect.y = yIndex * mapTileLength + 1;
+			rect.endX = rect.x + mapTileLength - 2;
+			rect.endY = rect.y + mapTileLength - 2;
+			rect.color = 0x00FFFFFF;
+			return rect;
+		case MapChar::CLYDE:
+			rect.x = xIndex * mapTileLength + 1;
+			rect.y = yIndex * mapTileLength + 1;
+			rect.endX = rect.x + mapTileLength - 2;
+			rect.endY = rect.y + mapTileLength - 2;
+			rect.color = 0xFFB851FF;
+			return rect;
+		default: throw WrongMapChar(c);
+		}
+	}
+
+	bool Nibbler::_collideWithWallMap(int x, int y, int w, int h)
+	{
+		for (const auto &i : this->_map) {
+			try {
+				auto rect = dynamic_cast<Drawables::Rectangle &>(*i.fallback);
+				if (rect.color != mapWallColor)
+					continue;
+				if (x + w <= rect.x
+					|| y + h <= rect.y
+					|| rect.endX <= x
+					|| rect.endY <= y) {
+					continue;
+				}
+				return true;
+			} catch (const std::bad_cast &) { }
+		}
+		return false;
+	}
+
+	Drawables::Sprite Nibbler::_getSpriteFromChar(char c, int xIndex, int yIndex)
+	{
+		Drawables::Sprite ret;
+
+		switch (c) {
+		case MapChar::WALL:
+			ret.fallback = std::make_shared<Drawables::Rectangle>(this->_getRectangleFromChar(c, xIndex, yIndex));
+			return ret;
+		case MapChar::HIDE_RECTANGLE:
+			ret.fallback = std::make_shared<Drawables::Rectangle>(this->_getRectangleFromChar(c, xIndex, yIndex));
+			return ret;
+		case MapChar::BIG_PACGUM:
+			ret.sizeY = mapTileLength;
+			ret.sizeX = mapTileLength;
+			ret.x = (xIndex * mapTileLength) + (ret.sizeX / 2);
+			ret.y = (yIndex * mapTileLength) + (ret.sizeY / 2);
+			ret.path = largePacgumFilename;
+			ret.rotation = 0;
+			ret.fallback = std::make_shared<Drawables::Circle>(this->_getCircleFromChar(c, xIndex, yIndex));
+			ret.color = 0;
+			return ret;
+		case MapChar::SMALL_PACGUM:
+			ret.sizeY = mapTileLength;
+			ret.sizeX = mapTileLength;
+			ret.x = (xIndex * mapTileLength) + (ret.sizeX / 2);
+			ret.y = (yIndex * mapTileLength) + (ret.sizeY / 2);
+			ret.path = smallPacgumFilename;
+			ret.rotation = 0;
+			ret.fallback = std::make_shared<Drawables::Circle>(this->_getCircleFromChar(c, xIndex, yIndex));
+			ret.color = 0;
+			return ret;
+		case MapChar::BLINKY:
+			ret.sizeY = mapTileLength;
+			ret.sizeX = mapTileLength;
+			ret.x = (xIndex * mapTileLength) + (ret.sizeX / 2);
+			ret.y = (yIndex * mapTileLength) + (ret.sizeY / 2);
+			ret.path = "assets/pacman/blinky.png";
+			ret.rotation = 0;
+			ret.fallback = std::make_shared<Drawables::Rectangle>(this->_getRectangleFromChar(c, xIndex, yIndex));
+			ret.color = 0xFF0000FF;
+			return ret;
+		case MapChar::INKY:
+			ret.sizeY = mapTileLength;
+			ret.sizeX = mapTileLength;
+			ret.x = (xIndex * mapTileLength) + (ret.sizeX / 2);
+			ret.y = (yIndex * mapTileLength) + (ret.sizeY / 2);
+			ret.path = "assets/pacman/inky.png";
+			ret.rotation = 0;
+			ret.fallback = std::make_shared<Drawables::Rectangle>(this->_getRectangleFromChar(c, xIndex, yIndex));
+			ret.color = 0x00FFFFFF;
+			return ret;
+		case MapChar::CLYDE:
+			ret.sizeY = mapTileLength;
+			ret.sizeX = mapTileLength;
+			ret.x = (xIndex * mapTileLength) + (ret.sizeX / 2);
+			ret.y = (yIndex * mapTileLength) + (ret.sizeY / 2);
+			ret.path = "assets/pacman/clyde.png";
+			ret.rotation = 0;
+			ret.fallback = std::make_shared<Drawables::Rectangle>(this->_getRectangleFromChar(c, xIndex, yIndex));
+			ret.color = 0xFFB851FF;
+			return ret;
+		default: throw WrongMapChar(c);
+		}
+	}
+
+	std::vector<Drawables::Sprite>::iterator Nibbler::_collideWithPacgumMap(int x, int y, int w, int h)
+	{
+		int index = -1;
+
+		for (const auto &sprite : this->_map) {
+			index++;
+			try {
+				if (sprite.path != largePacgumFilename && sprite.path != smallPacgumFilename)
+					continue;
+				if (x + w <= sprite.x - sprite.sizeX / 2
+				    || y + h <= sprite.y - sprite.sizeY / 2
+				    || sprite.x + sprite.sizeX / 2  <= x
+				    || sprite.y + sprite.sizeY / 2 <= y) {
+					continue;
+				}
+				return this->_map.begin() + index;
+			} catch (const std::bad_cast &) { }
+		}
+		return this->_map.end();
+	}
+
+	void Nibbler::_processPlayerMovement(unsigned int ticks)
+	{
+		double newX;
+		double newY;
+		int &moveX = this->_playerMovement.first;
+		int &moveY = this->_playerMovement.second;
+
+		if (this->_moves.moveX) {
+			moveX = this->_moves.moveX;
+		} else if (!this->_moves.moveX && this->_moves.moveY) {
+			moveY = this->_moves.moveY;
+		}
+		newX = this->pacmanSpeed * moveX * ticks;
+		newY = this->pacmanSpeed * moveY * ticks;
+
+		if (newX) {
+			if (this->_collideWithWallMap(newX + this->_playerPosition.first - (this->_playerDrawable.sizeX / 2),
+			                              this->_playerPosition.second - (this->_playerDrawable.sizeY / 2),
+			                              this->_playerDrawable.sizeX,
+			                              this->_playerDrawable.sizeY)) {
+				moveX = 0;
+				newX = 0;
+			}
+			this->_playerPosition.first += newX;
+		}
+		if (newY) {
+			if (this->_collideWithWallMap(this->_playerPosition.first - (this->_playerDrawable.sizeX / 2),
+			                              newY + this->_playerPosition.second - (this->_playerDrawable.sizeY / 2),
+			                              this->_playerDrawable.sizeX,
+			                              this->_playerDrawable.sizeY)) {
+				moveY = 0;
+				newY = 0;
+			}
+			this->_playerPosition.second += newY;
+		}
+
+		if (moveY) {
+			this->_playerDrawable.rotation = (moveY > 0) ? 90 : 270;
+		}
+		if (moveX) {
+			this->_playerDrawable.rotation = (moveX > 0) ? 0 : 180;
+		}
+
+		// Teleportation
+		if (moveX < 0 && this->_playerPosition.first < (mapOffsetTileX + 2) * mapTileLength) {
+			this->_playerPosition.first = 23 * mapTileLength - (mapTileLength / 2);
+		}
+		if (moveX > 0 && this->_playerPosition.first > (23 + mapOffsetTileX - 2) * mapTileLength) {
+			this->_playerPosition.first = (mapOffsetTileX + 1) * mapTileLength + (mapTileLength / 2);
+		}
+	}
+
+	void Nibbler::_processScore()
+	{
+		auto it = this->_collideWithPacgumMap(this->_playerPosition.first - (this->_playerDrawable.sizeX / 2),
+		                                      this->_playerPosition.second - (this->_playerDrawable.sizeY / 2),
+		                                      this->_playerDrawable.sizeX,
+		                                      this->_playerDrawable.sizeY);
+		if (it != this->_map.end()) {
+			this->_gameScore += (*it).path == this->largePacgumFilename ? 50 : 10;
+			this->_map.erase(it);
+		}
+
+		this->_scoreDrawable.text = "Score: " + std::to_string(this->_gameScore);
+	}
+
+	Drawables::Circle Nibbler::_getCircleFromChar(char c, int xIndex, int yIndex)
+	{
+		Drawables::Circle ret;
+
+		switch (c) {
+		case MapChar::BIG_PACGUM:
+			ret.size = 1;
+			ret.x = (xIndex * mapTileLength) + (mapTileLength / 2.);
+			ret.y = (yIndex * mapTileLength) + (mapTileLength / 2.);
+			ret.fallback = std::make_shared<Drawables::Rectangle>(this->_getRectangleFromChar(c, xIndex, yIndex));
+			ret.color = 0xFFFFFFFF;
+			return ret;
+		case MapChar::SMALL_PACGUM:
+			ret.size = 1;
+			ret.x = (xIndex * mapTileLength) + (mapTileLength / 2.);
+			ret.y = (yIndex * mapTileLength) + (mapTileLength / 2.);
+			ret.fallback = std::make_shared<Drawables::Rectangle>(this->_getRectangleFromChar(c, xIndex, yIndex));
+			ret.color = 0x777777FF;
+			return ret;
+		default: throw WrongMapChar(c);
+		}
+	}
+
+	bool Nibbler::_isGameEnded()
+	{
+		bool pacgumPresence = false;
+
+		for (const auto &sprite : this->_map) {
+			try {
+				if (sprite.path != largePacgumFilename && sprite.path != smallPacgumFilename) {
+					continue;
+				}
+				pacgumPresence = true;
+			} catch (const std::bad_cast &) { }
+		}
+		return !pacgumPresence;
+	}
+}
+
+extern "C" Arcade::ModInfo getHeader()
+{
+	Arcade::ModInfo info;
+	info.name = "Nibbler";
+	info.type = Arcade::ModInfo::GAME;
+	info.magicNumber = Arcade::MagicNumber;
+	return info;
+}
+
+extern "C" Arcade::IModule *getModule()
+{
+	return new Arcade::Nibbler::Nibbler;
+}
---
+++ sources/Games/Nibbler/Nibbler.hpp
@@ -0,0 +1,201 @@
+//
+// Created by Zoe Roux on 3/29/21.
+//
+
+#pragma once
+
+#include <map>
+#include "Common/IGameModule.hpp"
+#include "Common/Drawables/Line.hpp"
+#include "Common/Drawables/Circle.hpp"
+#include <array>
+
+namespace Arcade::Nibbler
+{
+	///! @brief The Nibbler game's module.
+	class Nibbler : public IGameModule
+	{
+	private:
+		//! @brief The ghost struct
+		struct Ghost {
+			int speed;
+			Drawables::Sprite drawable;
+		};
+
+		//! @brief Struct used to keep pending moves.
+		struct PendingMoves {
+			//! @brief X direction of the player
+			int moveX;
+			//! @brief Y direction of the player
+			int moveY;
+		};
+
+		enum MapChar {
+			//! @brief Represents a space, nothing should be drawned with it
+			//! @info It's used for padding
+			NOTHING = ' ',
+			//! @brief Representing a wall in the map
+			WALL = 'w',
+			//! @brief Representing a small pacgum in the map
+			SMALL_PACGUM = '.',
+			//! @brief Representing a big pacgum in the map
+			BIG_PACGUM = 'P',
+			//! @brief Ghost Blinky
+			BLINKY = 'B',
+			//! @brief Ghost Inky
+			INKY = 'I',
+			//! @brief Ghost Clyde
+			CLYDE = 'C',
+			//! @brief A black rectangle 1x4 (Used to hide some parts of the map)
+			HIDE_RECTANGLE = '_'
+		};
+
+		//! @brief The position of the player.
+		std::pair<double, double> _playerPosition;
+		//! @brief The movement of the player.
+		std::pair<int, int> _playerMovement;
+		//! @brief The player's drawable
+		Drawables::Sprite _playerDrawable;
+		//! @brief Information of all the ghosts
+		Ghost _ghosts;
+		//! @brief If the game has ended
+		bool _shouldClose;
+		//! @brief Game Score
+		long _gameScore = 0;
+		//! @brief Text element to draw the score to the screen
+		Drawables::Text _scoreDrawable;
+		//! @brief Resources needed by this game.
+		std::vector<std::pair<std::string, std::string>> _resources = {};
+		//! @brief Drawables that will be displayed.
+		std::vector<std::unique_ptr<Drawables::ADrawable>> _drawables = {};
+		//! @brief Sounds that will be started in the next frame
+		std::vector<Sound> _sounds = {};
+		//! @brief Game map
+		std::vector<Drawables::Sprite> _map = {};
+		//! @brief Pending moves.
+		PendingMoves _moves = {};
+		//! @brief The length of a map tile
+		static constexpr int mapTileLength = 4;
+		//! @brief The offset of the map in number of mapTileLength for the X axis
+		static constexpr int mapOffsetTileX = 1;
+		//! @brief The offset of the map in number of mapTileLength for the Y axis
+		static constexpr int mapOffsetTileY = 5;
+		//! @brief The color of the walls
+		//! @info This color is used to check if the block is collidable or not
+		static constexpr int mapWallColor = 0x0000FFFF;
+		//! @brief The speed of pacman (the player)
+		static constexpr float pacmanSpeed = .000020;
+		//! @brief Resource location for the large pacgum sprite
+		static constexpr std::string_view largePacgumFilename = "assets/pacman/large_pacgum.png";
+		//! @brief Resource location for the small pacgum sprite
+		static constexpr std::string_view smallPacgumFilename = "assets/pacman/small_pacgum.png";
+
+		//! @brief Gives the correct Drawables to be able to draw the map on the screen
+		//! @param map The vector of string to represent the actual map (one type of char represent a type/config of drawable)
+		//! @param vOffset The vertical offset in percentage
+		//! @param hOffset The horizontal offset in percentage
+		//! @info Offset can be set to a negative value the function will simply add the offset given to the offset calculated
+		//! @warning Sprites are only used for a "base drawable" some sprites will be purposely ill formed, in order to call the fallback
+		std::vector<Drawables::Sprite> _createMapFromVector(const std::vector<std::string> &map, int hOffset, int vOffset);
+		//! @brief Get the correct Rectangle and correctly filled from a char
+		//! @param c The template type
+		//! @param xIndex The x position of the char in the map
+		//! @param yIndex The y position of the char in the map
+		//! @warning This function uses hard coded templates for each char. This function should only be used with the map context in mind
+		//! @return A instance of a Rectangle
+		//! @throw WrongMapChar when no matching char is found
+		Drawables::Rectangle _getRectangleFromChar(char c, int xIndex, int yIndex);
+		//! @brief Tells if you colliding with one of the rectangle of the _map member
+		//! @param x The x percentage
+		//! @param y The y percentage
+		//! @param w The width in percentage
+		//! @param h The the height in percentage
+		//! @return True if the coords are colliding, otherwise false
+		bool _collideWithWallMap(int x, int y, int w, int h);
+		//! @brief Get the correct Sprite and correctly filled from a char
+		//! @param c The template type
+		//! @param xIndex The x position of the char in the map
+		//! @param yIndex The y position of the char in the map
+		//! @warning This function uses hard coded templates for each char. This function should only be used with the map context in mind
+		//! @return A instance of a Sprite
+		//! @throw WrongMapChar when no matching char is found
+		Drawables::Sprite _getSpriteFromChar(char c, int xIndex, int yIndex);
+		//! @brief Get the correct Circle and correctly filled from a char
+		//! @param c The template type
+		//! @param xIndex The x position of the char in the map
+		//! @param yIndex The y position of the char in the map
+		//! @warning This function uses hard coded templates for each char. This function should only be used with the map context in mind
+		//! @return A instance of a Circle
+		//! @throw WrongMapChar when no matching char is found
+		Drawables::Circle _getCircleFromChar(char c, int xIndex, int yIndex);
+		//! @brief Tells if you colliding with one of the pacgums of the _map member
+		//! @param x The x percentage
+		//! @param y The y percentage
+		//! @param w The width in percentage
+		//! @param h The the height in percentage
+		//! @return The pacgum you collided, otherwise
+		std::vector<Drawables::Sprite>::iterator _collideWithPacgumMap(int x, int y, int w, int h);
+		//! @brief Checks collisions and moves the player, rotate texture, etc...
+		//! @param ticks Delta time (number of ticks between two functions calls)
+		//! @info The movement will be kept until hitting an obstacle or a new input is detected
+		void _processPlayerMovement(unsigned int ticks);
+		//! @brief Checks the collision with Pacgums and ghosts and update the score
+		void _processScore();
+		//! @brief Start the game and initialise variables to their start values
+		void  _startGame();
+		//! @brief Tells if the game has ended
+		bool _isGameEnded();
+	public:
+		//! @brief Initialize this library. (Create windows & so on)
+		//! @return True if the initialization was successful. False otherwise.
+		bool init() override;
+
+		//! @brief Allow to say to the library to shutdown and free it's content
+		//! @return True if the shutdown was successful. False otherwise.
+		bool close() override;
+
+		//! @brief Should this module be closed?
+		//! @return True if this module should be closed. False otherwise.
+		bool shouldClose() override;
+
+		//! @brief Get the type of this module
+		ModInfo::Modtype getType() const override;
+
+		//! @brief Get resources to preload.
+		//! @info Only called once.
+		//! @return A vector of tuple of (type of resource, path of the resource).
+		const std::vector<std::pair<std::string, std::string>> &getResources() const override;
+
+		//! @brief Return a list of drawables to display.
+		//! @return The list of objects
+		const std::vector<std::unique_ptr<Drawables::ADrawable>> &getDrawables() override;
+
+		//! @brief Return a list of sounds to make.
+		//! @return The list of sounds
+		const std::vector<Sound> &getSounds() override;
+
+		//! @brief Advance the game of x seconds
+		//! @param tick The number of ticks that occurred since the last call.
+		void addTicks(unsigned tick) override;
+
+		//! @brief Restart the game.
+		void restart() override;
+
+		//! @brief Handle one event (A key press, a click, a close event...)
+		//! @param event The event to handle.
+		void handleEvent(Event &event) override;
+
+		//! @brief Get the score (used for saving/displaying)
+		unsigned long getScore() override;
+
+
+		//! @brief Default constructor
+		Nibbler() = default;
+		//! @brief Default copy constructor
+		Nibbler(const Nibbler &) = default;
+		//! @brief Default destructor
+		~Nibbler() override = default;
+		//! @brief Default assignment operator
+		Nibbler &operator=(const Nibbler &) = default;
+	};
+}
